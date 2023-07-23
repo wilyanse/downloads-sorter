fad4f5b93241d8d35e0bd2861a40576d9455b36b
Wilyanse <willjose51@gmail.com>
2023-07-23 08:17:09 +0800

    Finished project

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 This is a repository for a Python automation program that sorts my downloaded files from the downloads folder into the respective type folders each file should belong.
 
 This makes it easier for me to sort through files and to move them from one place to the other.
+
+Note that I am using an external hard drive so I added a situation wherein if I were unable to access my external hard drive, the files in my downloads folder will be sorted into the downloads directory instead.
+
+To use the python script, open the script and modify the download_folder and destination_folder to be the path of your download directory and the directory where you want to store the files, respectively.
+
+Editing the file_types dictionary will allow you to customize which directories should exist and which files are to be segregated into them.
--- downloads_sorter.py
+++ downloads_sorter.py
@@ -15,6 +15,30 @@ file_types = {
 download_folder = r"C:\Users\Admin\Downloads"
 destination_folder = r"E:\Downloads"
 
+# Configure logging to log both errors and info messages
+logging.basicConfig(level=logging.INFO)
+
+# Create a file handler to save log messages to the error_log.txt file
+log_file = "script_log.txt"
+log_handler = logging.FileHandler(log_file, delay=True)
+log_handler.setLevel(logging.INFO)  # Only log messages with ERROR level or above
+log_format = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+log_handler.setFormatter(log_format)
+
+# Add the file handler to the root logger
+logging.getLogger().addHandler(log_handler)
+
+# Check if the download folder exists
+if not os.path.exists(download_folder):
+    logging.error(f"The download folder '{download_folder}' does not exist.")
+    exit(1)
+
+# Check if the destination folder exists;
+# Sorts the files in the download folder if it does not
+if not os.path.exists(destination_folder):
+    logging.INFO(f"The destination folder does not exist, files will be sorted in the download folder instead")
+    destination_folder = download_folder
+
 # Iterate through files in the download folder
 def Categorize_Move(download_folder, destination_folder):
     for filename in os.listdir(download_folder):
@@ -44,6 +68,7 @@ def Categorize_Move(download_folder, destination_folder):
 
         dest_file = os.path.join(dest_folder, filename)
         shutil.move(src_file, dest_file)
+        logging.info(f"File '{filename}' was moved into '{destination_folder}'.")
 
 
 Categorize_Move(download_folder, destination_folder)
